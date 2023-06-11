22124ee3a260cef3ad3b5bba65a2d7dbdbf37274
unknown <dayathidayatr9p@gmail.com>
2023-06-11 13:58:11 +0700

    Ganti Config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bank_sampah_ci3/';
+$config['base_url'] = 'http://localhost/Bank_Sampah_CI3/';
 
 /*
 |--------------------------------------------------------------------------
